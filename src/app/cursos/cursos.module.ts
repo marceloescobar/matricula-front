33d82c62ac946cb1e9366951bd335bf7052e3c47
marceloescobar <escobar.marcelo@gmail.com>
2024-06-13 22:23:33 -0300

    migracao standalone component - remocao modulos

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -3,22 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosComponent } from './cursos/cursos.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { CursoFormComponent } from './curso-form/curso-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CursosListComponent } from './cursos-list/cursos-list.component';
 
 @NgModule({
     imports: [
-        CommonModule,
-        CursosRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CursosComponent,
-        CursoFormComponent,
-        CursosListComponent
-    ]
+    CommonModule,
+    CursosRoutingModule,
+    ReactiveFormsModule,
+    CursosComponent,
+    CursoFormComponent,
+    CursosListComponent
+]
 })
 export class CursosModule { }
